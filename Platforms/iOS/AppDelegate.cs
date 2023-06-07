67886647a538c5f477840fa24ece449ed09772ed
Stephan Arnas <stephan.arnas@gmail.com>
2023-06-07 19:04:34 +0200

    Init

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace DemoLayout;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
